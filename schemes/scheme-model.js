14e2f16a180bb1b354ddd44dea9bbe9e4f35d154
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-17 13:14:51 +0100

    Add functionality to get steps by scheme id

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -8,6 +8,10 @@ module.exports = {
     return db('schemes').where({schemeId}).first();
   },
   findSteps: function(schemeId) {
+    return db('steps')
+      .join('schemes', 'schemes.id', 'scheme_id')
+      .select('steps.id', 'schemes.scheme_name', 'steps.step_number', 'steps.instructions')
+      .where('scheme_id', schemeId);
 
   }
 }
