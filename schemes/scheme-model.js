3a758442543f7d7985c1e57a4eb48cbe60cd537c
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-17 13:03:20 +0100

    Add functionality to get schemes by ID

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,5 +3,11 @@ const db = require('../data/dbConfig');
 module.exports = {
   find: function() {
     return db('schemes');
+  },
+  findById: function(schemeId) {
+    return db('schemes').where({schemeId}).first();
+  },
+  findSteps: function(schemeId) {
+
   }
 }
